1314dfffc559485df0f3c243fd18d5bae7e460b9
Imad Ahmad <imadahmad97@yahoo.ca>
2022-11-18 13:42:49 -0700

    still figuring out flask

--- test_app.py
+++ test_app.py
@@ -1,12 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def home():
+@app.route('/')
+def upload():
     return render_template('index.html')
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('upload'))
+
+
+app.run(port=8000)
