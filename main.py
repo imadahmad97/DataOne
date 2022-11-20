c029f3caa6df03cdd5e493d57d1e54c6f8e50d41
Imad Ahmad <imadahmad97@yahoo.ca>
2022-11-19 22:27:01 -0700

    still not there yet

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from flask import Flask, render_template
+import pandas as pd
+import pandas_profiling
+from flask import Flask, render_template, send_file
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
 from werkzeug.utils import secure_filename
 import os
+from wtforms.validators import InputRequired
+
+def report_creator(uploaded_file):
+    raw_data = pd.read_csv(uploaded_file)
+    return pandas_profiling.ProfileReport(raw_data).to_file('/Users/imadahmad/repos/DataOne/RETURN_FOLDER/report.html')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
@@ -10,7 +17,7 @@ app.config['UPLOAD_FOLDER'] = 'static/files'
 
 
 class UploadFileForm(FlaskForm):
-    file = FileField("File")
+    file = FileField("File", validators = [InputRequired()])
     submit = SubmitField("Upload File")
 
 
@@ -23,7 +30,9 @@ def home():
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),
                                app.config['UPLOAD_FOLDER'],
                                secure_filename(file.filename)))
-        return "File has been uploaded!"
+        send_file(report_creator(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                                    app.config['UPLOAD_FOLDER'],
+                                    secure_filename(file.filename))))
     return render_template('index.html', form=form)
 
 
