cb431adb1f2a439d0656090f0bd176854b69ead4
Imad Ahmad <imadahmad97@yahoo.ca>
2022-11-19 21:31:25 -0700

    okay it can accept file uploads, now to output back the report

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import FileField, SubmitField
-from werkzeu.utils import secure_filename
+from werkzeug.utils import secure_filename
 import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['UPLOAD_FOLDER'] = 'static/files'
 
 
 class UploadFileForm(FlaskForm):
@@ -16,7 +17,13 @@ class UploadFileForm(FlaskForm):
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
-    form = UploadFielForm()
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        file = form.file.data
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                               app.config['UPLOAD_FOLDER'],
+                               secure_filename(file.filename)))
+        return "File has been uploaded!"
     return render_template('index.html', form=form)
 
 
