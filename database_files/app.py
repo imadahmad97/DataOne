e5f796771b83614ccfcff739c284c57abd0e84e9
Imad Ahmad <imadahmad97@yahoo.ca>
2022-12-12 00:16:34 -0700

    its working

--- database_files/app.py
+++ database_files/app.py
@@ -1,6 +1,5 @@
-from io import BytesIO
 from script import analyze
-
+from io import BytesIO
 from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy 
 
@@ -18,16 +17,15 @@ class Upload(db.Model):
 def index():
     if request.method == 'POST':
         file = request.files['file']
-
+        print(type(file))
         upload = Upload(filename=file.filename, data=file.read())
         db.session.add(upload)
         db.session.commit()
-        
-        return f'Uploaded: {file.filename}'
-    return render_template(analyze(upload.data))
+        data_for_function = BytesIO(upload.data)
+        analyze(data_for_function, upload.id)
+        return render_template(f'{upload.id}.html')
+    else:
+        return render_template('index.html')
 
-@app.route('/download/<upload_id>')
-def download(upload_id):
-    upload = Upload.query.filter_by(id=upload_id).first()
-    return send_file(BytesIO(upload.data), attachment_filename=upload.filename, as_attachment=True)
+app.run()
 
--- database_files/script.py
+++ database_files/script.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
-from flask import render_template
 
-def analyze(fl, idx):
+def analyze(fl, num):
     raw = pd.read_csv(fl)
     profile = ProfileReport(raw, title = "Report", explorative=True)
-    profile.to_file(f'database_files/templates/{idx}.html')
+    profile.to_file(f'database_files/templates/{num}.html')
