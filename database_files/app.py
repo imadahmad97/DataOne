a0f9fca3d85960396123c9d5a14fab1a7030969f
Imad Ahmad <imadahmad97@yahoo.ca>
2022-12-12 01:36:08 -0700

    large file bug fixed

--- database_files/app.py
+++ database_files/app.py
@@ -4,26 +4,25 @@ from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:mdbpassword@databaseone.cyalagmldkju.us-west-2.rds.amazonaws.com/mariadbtest'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://admin:mdbpassword@databaseone.cyalagmldkju.us-west-2.rds.amazonaws.com/mariadbtest'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# db = SQLAlchemy(app)
 
-class Upload(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(50))
-    data = db.Column(db.LargeBinary)
+# class Upload(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     filename = db.Column(db.String(50))
+#     data = db.Column(db.LargeBinary)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         file = request.files['file']
-        print(type(file))
-        upload = Upload(filename=file.filename, data=file.read())
-        db.session.add(upload)
-        db.session.commit()
-        data_for_function = BytesIO(upload.data)
-        analyze(data_for_function, upload.id)
-        return render_template(f'{upload.id}.html')
+        # upload = Upload(filename=file.filename, data=file.read())
+        # db.session.add(upload)
+        # db.session.commit()
+        data_for_function = BytesIO(file.read())
+        analyze(data_for_function)
+        return render_template(f'{data_for_function}.html')
     else:
         return render_template('index.html')
 
--- database_files/script.py
+++ database_files/script.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-def analyze(fl, num):
+def analyze(fl):
     raw = pd.read_csv(fl)
     profile = ProfileReport(raw, title = "Report", explorative=True)
-    profile.to_file(f'database_files/templates/{num}.html')
+    profile.to_file(f'database_files/templates/{fl}.html')
